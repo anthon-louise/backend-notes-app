838df4e98c1bd93aa92ec61f672c89025c1a65e0
anthon-louise <anthonlouisenoynay123@gmail.com>
2025-07-27 04:12:45 +0800

    feature: folder crud

--- controllers/folder.controllers.js
+++ controllers/folder.controllers.js
@@ -4,17 +4,84 @@ const validationSchema = require('../validations/validationSchema')
 
 const createFolder = asyncHandler(async (req, res) => {
     const value = await validationSchema.folderSchema.validateAsync(req.body)
-    const {name} = value
+    const { name } = value
     const userId = req.user.userId
 
-    const [result] = await pool.query(`
+    await pool.query(`
         INSERT INTO folders (name, user_id) VALUES (?, ?)
         `, [name, userId])
-    
-    res.json({message: 'Folder created'})
+
+    res.json({ message: 'Folder created' })
+})
+
+const getFolder = asyncHandler(async (req, res) => {
+    const { userId } = req.user
+
+    const [rows] = await pool.query(`
+        SELECT * FROM folders WHERE user_id=?
+        `, [userId])
+
+    res.json(rows)
+})
+
+
+// const getFolderById = asyncHandler(async (req, res) => {
+//     const value = await validationSchema.idSchema.validateAsync(req.params)
+//     const folderId = value.id
+
+
+// })
+
+const deleteFolder = asyncHandler(async (req, res) => {
+    const value = await validationSchema.idSchema.validateAsync(req.params)
+    const folderId = value.id
+    const { userId } = req.user
+
+    const [rows] = await pool.query(`
+        SELECT * FROM folders WHERE id=? AND user_id=?
+        `, [folderId, userId])
+    if (rows.length === 0) {
+        const err = new Error('No folder found')
+        err.status = 404
+        throw err
+    }
+
+    await pool.query(`
+        DELETE FROM folders WHERE id=? AND user_id=?
+        `, [folderId, userId])
+
+    res.json({ message: 'Deleted' })
+})
+
+const updateFolder = asyncHandler(async (req, res) => {
+    const bodyValue = await validationSchema.folderSchema.validateAsync(req.body)
+    const { name } = bodyValue
+
+    const idValue = await validationSchema.idSchema.validateAsync(req.params)
+    const folderId = idValue.id
+
+    const {userId} = req.user
+
+    const [rows] = await pool.query(`
+        SELECT * FROM folders WHERE id=? AND user_id=?
+        `, [folderId, userId])
+    if (rows.length === 0) {
+        const err = new Error('No folder found')
+        err.status = 404
+        throw err
+    }
+
+    await pool.query(`
+        UPDATE folders SET name=? WHERE id=? AND user_id=?
+        `, [name, folderId, userId])
+
+    res.json({message: 'Update success'})
 })
 
 module.exports = {
-    createFolder
+    createFolder,
+    getFolder,
+    deleteFolder,
+    updateFolder
 }
 
--- routes/folder.routes.js
+++ routes/folder.routes.js
@@ -4,5 +4,9 @@ const folderRoutes = require('../controllers/folder.controllers')
 const auth = require('../middlewares/auth')
 
 router.post('/', auth, folderRoutes.createFolder)
+router.get('/', auth, folderRoutes.getFolder)
+// router.get('/:id', auth, folderRoutes.getFolderById)
+router.delete('/:id', auth, folderRoutes.deleteFolder)
+router.put('/:id', auth, folderRoutes.updateFolder)
 
 module.exports = router
